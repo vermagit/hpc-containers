df6f034c5ca7502cb02dd06f1135f33da18cc869
Aman Verma <amverma@microsoft.com>
2019-04-15 15:19:13 -0700

    Install Singularity on Ubuntu

--- singularity/setup-singularity.sh
+++ singularity/setup-singularity.sh
@@ -1,17 +1,35 @@
-. ./common_dirs.sh
+if [ ! $(. ./common_dirs.sh) ]; then
+  INSTALL_DIR=$HOME
+fi
 
 # Install Singularity
 
+# Find distro
+DISTRO=$(awk -F= '/^ID=/{print $2}' /etc/os-release)
+
 # Pre-requisites
-sudo yum install -y git
-sudo yum install -y openssl-devel libuuid-devel
-sudo yum install -y squashfs-tools
+case "$DISTRO" in
+  *cent* | *rhel*)
+    sudo yum install -y git \
+      openssl-devel libuuid-devel \
+      squashfs-tools
+    ;;
+  *ubuntu*)
+    sudo apt-get install -y build-essential \
+      libssl-dev uuid-dev libseccomp-dev \
+      pkg-config \
+      squashfs-tools
+esac
 
 # GO
-wget -nc --retry-connrefused --tries=3 --waitretry=5 https://dl.google.com/go/go1.11.4.linux-amd64.tar.gz
-tar -xzvf go1.11.4.linux-amd64.tar.gz
+ROOT=/usr/local
+GOROOT=$ROOT/go
 GOPATH=$INSTALL_DIR/go
-export PATH=${PATH}:${GOPATH}/bin
+export VERSION=1.12.4 OS=linux ARCH=amd64
+wget -nc --retry-connrefused --tries=3 --waitretry=5 https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz
+sudo tar -C $ROOT -xzvf go$VERSION.$OS-$ARCH.tar.gz
+export PATH=${PATH}:${GOROOT}/bin:${GOPATH}/bin
+go get -u github.com/golang/dep/cmd/dep
 
 # Singularity
 mkdir -p $GOPATH/src/github.com/sylabs
@@ -20,6 +38,6 @@ git clone https://github.com/sylabs/singularity.git
 cd singularity
 ./mconfig --prefix=$INSTALL_DIR/singularity
 cd builddir
-make -j 16 && make install
+make -j 16 && sudo make install
 
 popd
